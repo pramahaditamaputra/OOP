1d23d6521493351764f5d36eace70bb5f711281f
PramahadiTamaPutra <pramahaditamaputra@outlook.com>
2019-05-15 13:21:12 +0700

    Initial Commit

---
+++ OOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OOP
+//
+//  Created by Pramahadi Tama Putra on 15/05/19.
+//  Copyright © 2019 Pramahadi Tama Putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
